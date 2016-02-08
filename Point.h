3ab83e5b8c1036d9365762fd6de676226ad6322b
Fernando Maravi <FM90805@hotmail.com>
2016-02-07 23:40:43 -0700

    Point.h
    I added the third dimension!

--- Point.h
+++ Point.h
@@ -7,20 +7,24 @@
 class Point {
     
 private:
-    double x, y;
+    double x, y, z;  // z adds the third deminsion point 
 
 public:
     // Constructors
     Point();                      // default constructor
-    Point(double x, double y);    // two-argument constructor
-
+    Point(double x, double y, double z); // z is added as the thrid argument // two argument constructor 
+    
     // Mutator methods
     void setX(double newX);
     void setY(double newY);
+    void setZ(double newZ); // mutator z was addded to the mutator methods
+    
+    double distanceTo(const Point &other) const; // non member function
 
     // Accessor methods
     double getX() const;
     double getY() const;
+    double getZ() const; // accessor method for Z
     
 };
 
